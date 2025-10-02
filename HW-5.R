deeeb0288212962c16b17a3aac6253c076046618
nathanlin-uw <ntlin@wisc.edu>
2025-10-02 15:18:37 -0500

    Problem 2!

--- HW-5.R
+++ HW-5.R
@@ -3,6 +3,7 @@
 
 #Load libraries
 library(readxl)
+library(writexl)
 
 ##Problem 1 ----
 
@@ -16,5 +17,24 @@ list_fish <- list(fish_csv, fish_xlsx, fish_rds) #combine into a list to use lap
 #display first with lapply
 lapply(X = list_fish, FUN = head, n = 5)
 
+
 ##Problem 2 ----
 
+# We will use the fish_csv dataset
+# Save as csv, xlsx, and rds -- make the Outputs folder first
+# dir.create("./Outputs")
+write.csv(fish_csv, "Outputs/second_fish.csv", row.names=FALSE)
+write_xlsx(fish_csv, "Outputs/second_fish.xlsx")
+saveRDS(fish_csv, "Outputs/second_fish.rds")
+
+# Grab the file names
+second_fish_filenames_list <- list.files("Outputs", full.names=TRUE)
+
+# Check the file info to compare file sizes
+lapply(X=second_fish_filenames_list, FUN=file.info)
+# The csv was 12128 bytes, the xlsx was 13870 bytes, and the rds was 3033 bytes.
+
+# It looks like csv is the best for sharing because it's not a huge file size and viewable in R or with most other platforms. For compact storage RDS is the smallest.
+
+
+##Problem 3 ----
